9c9a3e9b35df14a0c254f4f07cae0d951a5e7b05
DanielRGilles <danryangilles@gmail.com>
2022-01-14 15:40:40 -0800

    initial commit and tailwind css

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className='bg-blue-500 m-11 p-5 text-4xl container'   > lorem ipsum Drognan told me about Hratli when we were in Lut Gholein. He said that he weaves magic into his forge and produces mystical weapons and armor.
+
+His skills could be quite useful to you.This is the tailwind example</div>
+        <a
+          className=" App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
