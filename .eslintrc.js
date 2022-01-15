d939f7e25e3c10c8c983571f8704b8cb328fbadf
DanielRGilles <danryangilles@gmail.com>
2022-01-14 16:12:09 -0800

    added eslint rule to fix red madness

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error" {"singleQuote": true, "parser": "flow"},
+      {
+        printWidth: 80,
+        trailingComma: "es5",
+        semi: false,
+        jsxSingleQuote: true,
+        singleQuote: true,
+        useTabs: true,
+      },
+    ],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -11,7 +11,7 @@ function App() {
         </p>
         <div className='bg-blue-500 m-11 p-5 text-4xl container'   > lorem ipsum Drognan told me about Hratli when we were in Lut Gholein. He said that he weaves magic into his forge and produces mystical weapons and armor.
 
-His skills could be quite useful to you.This is the tailwind example</div>
+        His skills could be quite useful to you.This is the tailwind example</div>
         <a
           className=" App-link"
           href="https://reactjs.org"
