1eb183ce72e4ecf5bb35e573eddf46e003b52d7e
KDSigel <heykarl@gmail.com>
2022-01-18 09:37:13 -0800

    fixed tests again

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { supabaseAnonKey, supabaseUrl } from './services/supabaseClient';
+
 
 test.skip('renders learn react link', () => {
   render(
   <MemoryRouter>
-  <App />
+  <App supabaseUrl={supabaseUrl} supabaseAnonKey={supabaseAnonKey} />
   </MemoryRouter>
   );
   const linkElement = screen.getByText(/Supabase/i);
