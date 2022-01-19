8fb3661602a7c02070790cd8fa709f0742dd14c7
ProsperieEli <ElijahProsperie@gmail.com>
2022-01-19 17:30:02 -0500

    storing

--- src/components/Calendar/CalendarDay.jsx
+++ src/components/Calendar/CalendarDay.jsx
@@ -7,7 +7,6 @@ export default function CalendarDay() {
         Your daily workouts consist of:
         <ul>
           <li></li>
-          <li></li>
         </ul>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Add new workout.
