c1a362e2e8ba48fca19fea759a335390b32c3088
Julius Franklin <jjfranklin88@outlook.com>
2022-01-19 14:57:46 -0500

    adding workout functionality

---
+++ src/context/WorkoutContext.jsx
@@ -0,0 +1,24 @@
+import { useContext, createContext, useState } from "react";
+
+
+
+const workoutCtx = createContext();
+
+
+export const WorkoutProvider = ({ children }) => {
+    const [workouts, setWorkouts] = useState([])
+
+return <workoutCtx.Provider value={{workouts, setWorkouts}}>{children}</workoutCtx.Provider>
+}
+
+
+
+export const useWorkout = () => {
+    const context = useContext(workoutCtx)
+
+    if(context === undefined){
+        throw new Error('Invalid Provider')
+    }
+
+    return context;
+}
--- src/views/AllWorkouts/AllWorkouts.jsx
+++ src/views/AllWorkouts/AllWorkouts.jsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { getWorkouts } from '../../services/wgerClient'
 import Workout from '../../components/Workout'
 import { addWorkout } from '../../services/supabaseClient'
+import { useWorkout } from '../../context/WorkoutContext'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function AllWorkouts() {
 
-    const [workouts, setWorkouts] = useState([])
+    const [exercises, setExercises] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const {setWorkouts} = useWorkout();
 
     useEffect(() => {
 
         const getAllWorkouts = async () => {
             const allWorkouts = await getWorkouts()
-            setWorkouts(allWorkouts)
+            setExercises(allWorkouts)
             setIsLoading(false)
             console.log(allWorkouts)
         }
@@ -20,8 +23,12 @@ export default function AllWorkouts() {
 
     }, [])
 
+
+
     const handleAdd = async (workout) => {
         await addWorkout({ workout_name: workout.name, workout_description: workout.description, workout_category: workout.category, workout_id: workout.id  })
+
+        setWorkouts((prevState) => [...prevState, workout])
 
     }
 
@@ -29,9 +36,10 @@ export default function AllWorkouts() {
 
     return (
         <div>
+            <Link to='/calendarday'>Back To Day</Link>
             {isLoading ? <h1 className='text-xl font-bold'>Loading...</h1> :
             <ul>
-            {workouts.map((workout) => <li key={workout.id}>
+            {exercises.map((workout) => <li key={workout.id}>
                 <Workout name={workout.name} description={workout.description} category={workout.category} onClick={() => handleAdd(workout)} />
             </li>)}</ul>
             }
