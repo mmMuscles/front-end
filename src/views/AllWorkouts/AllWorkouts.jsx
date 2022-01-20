8beb61b6ccddf913731a64e30f334bc9b135b825
Julius Franklin <jjfranklin88@outlook.com>
2022-01-20 16:04:53 -0500

    adding pagination conditional buttons

--- src/views/AllWorkouts/AllWorkouts.jsx
+++ src/views/AllWorkouts/AllWorkouts.jsx
@@ -15,6 +15,7 @@ export default function AllWorkouts() {
     const [exercises, setExercises] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const { workouts, setWorkouts } = useWorkout();
+    const [offset, setOffset] = useState(0)
     const location = useLocation();
     const date = location.search.split('=')[1]
     const {user} = useUser()
@@ -22,7 +23,7 @@ export default function AllWorkouts() {
     useEffect(() => {
 
         const getAllWorkouts = async () => {
-            const allWorkouts = await getWorkouts()
+            const allWorkouts = await getWorkouts(offset)
             setExercises(allWorkouts)
             const getArray = await getWorkoutArray(date, user.id)
             setWorkouts(getArray);
@@ -32,7 +33,7 @@ export default function AllWorkouts() {
         getAllWorkouts()
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [date])
+    }, [date, offset])
 
     const handleAdd = async (workout) => {
        await addWorkout({ user_id: user.id, workouts: JSON.stringify([workout.id]), date: date})
@@ -55,11 +56,13 @@ export default function AllWorkouts() {
 
         <Link to='/calendar?date='><button>Back To Day</button></Link>
 
+            {offset > 0 ? <button  onClick={() => setOffset(offset - 10)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Prev Page</button> : null}
+            {exercises.length >= 10 ? <button onClick={() => setOffset(offset + 10)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Next Page</button> : null}
             {isLoading ? <h1 className='text-xl font-bold'>Loading...</h1> :
 
             <ul>
-            {exercises.map((workout) => <Card> <li key={workout.id}>
-                <Workout name={workout.name} description={workout.description} category={workout.category}/>{workouts.includes(workout.id) ? <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleRemove(workout.id)}>Remove</button> : <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleAdd(workout)}>Add</button>}
+            {exercises.map((workout) => <Card key={workout.id}> <li key={workout.id}>
+                <Workout name={workout.name} description={workout.description} category={workout.category}/>{workouts.includes(workout.id) ? <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleRemove(workout.id)}>Remove</button> : <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => handleAdd(workout)}><span>Add</span><span>+</span></button>}
             </li></Card>)}</ul>
             }
         </div>
