2019b2290fe41d4b122f70e92679c004860a4c71
ProsperieEli <ElijahProsperie@gmail.com>
2022-01-20 13:25:26 -0500

    storing

--- src/components/Calendar/CalendarHome.jsx
+++ src/components/Calendar/CalendarHome.jsx
@@ -8,7 +8,7 @@ import { useWorkout } from "../../context/WorkoutContext";
 export default function CalendarHome() {
   const [date, setDate] = useState(new Date());
   const { workouts } = useWorkout();
-  const selectedDate = moment(date).format("YYYY-MM-DD")
+  const selectedDate = moment(date).format("YYYY-MM-DD");
 
   const handleData = (e) => {
     setDate(e);
@@ -19,17 +19,32 @@ export default function CalendarHome() {
       <Calendar value={date} onChange={handleData} />
       Workout for <b>{selectedDate}</b>
       <p>Today's focus section is: {<CalenderList />}</p>
-      Your daily workouts consist of:
-      {workouts &&
-      <ul>
-        {workouts.map((workout) => <li>{workout.name}</li>)}
-      </ul>}
-      <Link to={`/allworkouts?date=${selectedDate}`}>
+      {!workouts ? (
+        <p>'You have no workouts scheduled for today!'</p>
+      ) : (
+        <div>
+          {" "}
+          Your daily workouts consist of
+          {workouts && (
+            <ul>
+              {workouts.map((workout) => (
+                <li>{workout.name}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
+      {!workouts ? (
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Adjust Your Day.
+          Add New Workouts
         </button>
-      </Link>
-      {console.log(workouts)}
+      ) : (
+        <Link to={`/allworkouts?date=${selectedDate}`}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add workouts
+          </button>
+        </Link>
+      )}
     </>
   );
 }
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Card  from './Card/Card'
+import React from "react";
+import Card from "./Card/Card";
 
 export default function Workout({ name, description, category, onClick }) {
-
-    return (
-        <Card>
-        <div>
-            <h1 className='text-xl font-bold' >{name}</h1>
-            <p dangerouslySetInnerHTML={{__html: description}}></p>
-            <p><b>Category:</b> {category}</p>
-            <button onClick={onClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add</button>
-        </div>
-        </Card>
-
-    )
+  return (
+    <Card>
+      <div>
+        <h1 className="text-xl font-bold">{name}</h1>
+        <p dangerouslySetInnerHTML={{ __html: description }}></p>
+        <p>
+          <b>Category:</b> {category}
+        </p>
+        <button
+          onClick={onClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Add
+        </button>
+      </div>
+    </Card>
+  );
 }
